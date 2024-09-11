60ab4a57e9d0fd3ba56d0fe8b3398b4802de0506
Qthulhu42 <34537274+Qthulhu42@users.noreply.github.com>
2024-09-11 16:22:33 +1000

    Fix parse issues with whitespace

--- parse_queens.py
+++ parse_queens.py
@@ -4,9 +4,10 @@ with open('queens.html','r') as f:
 
 import numpy as np
 grid = [s for s in f.split('cell-color-')[1:]]
-print([s[:10] for s in grid])
+grid = [s.split(' ',1)[0] for s in grid]
 grid = np.array([int(s.split('-',1)[0]) for s in grid])
 width = int(np.sqrt(len(grid)))
 grid = grid.reshape(width,width)
+print(grid)
 with open('grid.lp','w') as f:
     f.write('\n'.join([f'cell({i},{j},{x}).'for i,row in enumerate(grid) for j,x in enumerate(row)]))
